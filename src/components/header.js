6b98be2b67212c88de31e274ff5a5bc2a7c75208
Anna Ira <annairahurnaus@mytum.de>
2020-04-15 22:13:55 +0200

    add globa and component style

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { navigate } from "gatsby"
+import headerStyles from './header.module.css'
 
 const Header = ({title}) => {
   return (
     <div>
-      <h1>{title}</h1>
+      <h1 className={headerStyles.example}>{title}</h1>
     </div>
   )
 }
