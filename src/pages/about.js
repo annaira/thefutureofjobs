9a95710d7ea12083444a014652114846fd67bafa
Anna Ira <annairahurnaus@mytum.de>
2020-04-15 23:24:04 +0200

    added about us and contact page

---
+++ src/pages/about.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+export default () => {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="row justify-content-md-center">
+          <div className="col-10">
+            <h1>About Us</h1>
+            <p>
+              Finished her are its honoured drawings nor. Pretty see mutual
+              thrown all not edward ten. Particular an boisterous up he
+              reasonably frequently. Several any had enjoyed shewing studied
+              two. Up intention remainder sportsmen behaviour ye happiness. Few
+              again any alone style added abode ask. Nay projecting unpleasing
+              boisterous eat discovered solicitude. Own six moments produce
+              elderly pasture far arrival. Hold our year they ten upon.
+              Gentleman contained so intention sweetness in on resolving.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
